9fea857289593c3bb4d8be43d36ca003f6d65d8d
Falasefemi2 <femifalase228@gmail.com>
2024-05-16 16:29:54 +0100

    Installed netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
